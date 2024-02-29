3baa8d801b890893f4b7e1f4b49ff626bf7feb21
Nitish Singh <nitishsingh925@gmail.com>
2024-03-01 00:44:15 +0530

    courses

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,4 @@
 import FeaturedCourses from "@/components/FeaturedCourses";
-import Footer from "@/components/Footer";
 import HeroSection from "@/components/HeroSection";
 import Instructors from "@/components/Instructors";
 import TestimonialCards from "@/components/TestimonialCards";
@@ -15,7 +14,6 @@ export default function Home() {
       <TestimonialCards />
       <UpcomingWebinars />
       <Instructors />
-      <Footer />
     </main>
   );
 }
