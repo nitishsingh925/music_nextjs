8ad9929c5bd7d6a27817a222e46a674e5771eea0
Nitish Singh <nitishsingh925@gmail.com>
2024-03-01 16:04:02 +0530

    contact

--- src/app/courses/page.tsx
+++ src/app/courses/page.tsx
@@ -1,58 +1,78 @@
+// Import necessary modules and components
 "use client";
 import courseData from "@/data/music_courses.json";
 import { CardBody, CardContainer, CardItem } from "@/components/ui/3d-card";
 import Image from "next/image";
+import { LampContainer } from "@/components/ui/lamp";
+import { motion } from "framer-motion";
+
 const pages = () => {
   return (
-    <div className="min-h-screen bg-black py-12 pt-36">
-      <h1 className="text-lg md:text-4xl text-center font-sans font-bold mb-8 text-white">
-        All courses ({courseData.courses.length})
-      </h1>
-      <div className="flex flex-wrap justify-center">
-        {courseData.courses.map((course) => (
-          <CardContainer className="inter-var mx-4" key={course.id}>
-            <CardBody className="bg-gray-50 relative group/card  dark:hover:shadow-2xl dark:hover:shadow-emerald-500/[0.1] dark:bg-black dark:border-white/[0.2] border-black/[0.1] w-auto sm:w-[30rem] h-auto rounded-xl p-6 border  ">
-              <CardItem
-                translateZ="50"
-                className="text-xl font-bold text-neutral-600 dark:text-white"
-              >
-                {course.title}
-              </CardItem>
-              <CardItem
-                as="p"
-                translateZ="60"
-                className="text-neutral-500 text-sm max-w-sm mt-2 dark:text-neutral-300"
-              >
-                {course.description}
-              </CardItem>
-              <CardItem translateZ="100" className="w-full mt-4">
-                <Image
-                  priority={false}
-                  src={course.image}
-                  height="1000"
-                  width="1000"
-                  className="h-60 w-full object-cover rounded-xl group-hover/card:shadow-xl"
-                  alt={course.slug}
-                />
-              </CardItem>
-              <div className="flex justify-between items-center mt-20">
+    <div className="-mt-14">
+      <LampContainer>
+        <motion.div
+          initial={{ opacity: 0.5, y: 100 }}
+          whileInView={{ opacity: 1, y: 0 }}
+          transition={{
+            delay: 0.3,
+            duration: 1,
+            ease: "easeInOut",
+          }}
+        >
+          <h1 className="mt-20 bg-gradient-to-br from-slate-300 to-slate-500 py-4 bg-clip-text text-center text-4xl font-medium tracking-tight text-transparent md:text-7xl">
+            All courses ({courseData.courses.length})
+          </h1>
+        </motion.div>
+      </LampContainer>
+
+      {/* Rest of your code */}
+      <div className="bg-slate-950 -mt-48 py-12">
+        <div className="flex flex-wrap justify-center">
+          {courseData.courses.map((course) => (
+            <CardContainer className="inter-var mx-4" key={course.id}>
+              <CardBody className="bg-gray-50 relative group/card  dark:hover:shadow-2xl dark:hover:shadow-emerald-500/[0.1] dark:bg-black dark:border-white/[0.2] border-black/[0.1] w-auto sm:w-[30rem] h-auto rounded-xl p-6 border  ">
                 <CardItem
-                  translateZ={20}
-                  as="button"
-                  className="px-4 py-2 rounded-xl text-xs font-normal dark:text-white"
+                  translateZ="50"
+                  className="text-xl font-bold text-neutral-600 dark:text-white"
                 >
-                  {course.instructor}
+                  {course.title}
                 </CardItem>
-                <CardItem translateZ={20}>
-                  <button className="px-8 py-2 rounded-full relative  text-white text-sm hover:shadow-2xl hover:shadow-white/[0.1] transition duration-200 border border-slate-600">
-                    <div className="absolute inset-x-0 h-px w-1/2 mx-auto -top-px shadow-2xl  bg-gradient-to-r from-transparent via-teal-500 to-transparent" />
-                    <span className="relative z-20">{course.price}</span>
-                  </button>
+                <CardItem
+                  as="p"
+                  translateZ="60"
+                  className="text-neutral-500 text-sm max-w-sm mt-2 dark:text-neutral-300"
+                >
+                  {course.description}
                 </CardItem>
-              </div>
-            </CardBody>
-          </CardContainer>
-        ))}
+                <CardItem translateZ="100" className="w-full mt-4">
+                  <Image
+                    priority={false}
+                    src={course.image}
+                    height="1000"
+                    width="1000"
+                    className="h-60 w-full object-cover rounded-xl group-hover/card:shadow-xl"
+                    alt={course.slug}
+                  />
+                </CardItem>
+                <div className="flex justify-between items-center mt-20">
+                  <CardItem
+                    translateZ={20}
+                    as="button"
+                    className="px-4 py-2 rounded-xl text-xs font-normal dark:text-white"
+                  >
+                    {course.instructor}
+                  </CardItem>
+                  <CardItem translateZ={20}>
+                    <button className="px-8 py-2 rounded-full relative  text-white text-sm hover:shadow-2xl hover:shadow-white/[0.1] transition duration-200 border border-slate-600">
+                      <div className="absolute inset-x-0 h-px w-1/2 mx-auto -top-px shadow-2xl  bg-gradient-to-r from-transparent via-teal-500 to-transparent" />
+                      <span className="relative z-20">{course.price}</span>
+                    </button>
+                  </CardItem>
+                </div>
+              </CardBody>
+            </CardContainer>
+          ))}
+        </div>
       </div>
     </div>
   );
